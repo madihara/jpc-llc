c93b02e6f9ddb0aa063bc56d2e5d549bae078450
Madison Haradine <madisonharadine@gmail.com>
2020-07-14 17:39:50 -0400

    button

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -5,11 +5,11 @@ import styled from 'styled-components'
 
 const ContactForm = () => {
   return(
-    <form method="POST" data-netlify="true" netlify-honeypot="bot-field" name="contact-form">
+    <form method="POST" data-netlify="true" name="contact-form">
       <input type= "name" name="name" id="name" placeholder="name" />
       <input type= "email" name="email" id="email" placeholder="email" />
       <TextArea name="message" id="message" placeholder="tell us more about your project needs and timelines."></TextArea>
-      <button type="submit">Send</button>
+      <button type="submit" name="button">Send</button>
      
     </form>
   )
